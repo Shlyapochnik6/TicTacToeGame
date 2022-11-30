30de244ad8655a9323af868ca8efa952663dd4db
Arseniy Kalenik <dhhxdjsjdh@gmail.com>
2022-11-30 14:13:14 +0300

    add entities

---
+++ TicTacToeGame.Domain/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace TicTacToeGame.Domain;
+
+public class Player
+{
+    public long Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string GameChip { get; set; } = "";
+
+    public List<TicTacToe> TicTacToeGames { get; set; }
+}
---
+++ TicTacToeGame.Domain/TicTacToe.cs
@@ -0,0 +1,16 @@
+﻿namespace TicTacToeGame.Domain;
+
+public class TicTacToe
+{
+    public long Id { get; set; }
+    
+    public string[] Field { get; set; }
+    
+    public Guid ConnectionId { get; set; }
+    
+    public string GameStatus { get; set; }
+    
+    public string PlayerMove { get; set; }
+    
+    public List<Player> Players { get; set; }
+}
