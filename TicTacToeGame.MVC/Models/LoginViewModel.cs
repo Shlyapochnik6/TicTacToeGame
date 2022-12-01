58bb61d9620f62d12eb8a9781802e4c807789a0b
Arseniy Kalenik <dhhxdjsjdh@gmail.com>
2022-12-01 15:24:53 +0300

    change login page

--- TicTacToeGame.MVC/Models/LoginViewModel.cs
+++ TicTacToeGame.MVC/Models/LoginViewModel.cs
@@ -5,6 +5,5 @@ namespace TicTacToeGame.MVC.Models;
 public class LoginViewModel
 {
     [Required(ErrorMessage = "You haven't entered your name")]
-    [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "Only alphabets and numbers allowed.")]
     public string Name { get; set; }
 }
